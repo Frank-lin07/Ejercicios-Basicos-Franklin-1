5a41e09bb5556a878d841d500a6436453da4cf50
Franklin Grisales Castrillon <franklitonadador123@gmail.com>
2026-02-28 20:10:01 -0500

    Inicio de proceso en java

--- funciones.js
+++ funciones.js
@@ -1,3 +1,6 @@
 console.log("esta conectado el JS")
+//document.querySelector(Button).addEventListener("click", calcularIMC);
+
+document.getElementById("calcular-IMC").addEventListener("click", calcularIMC);
 
 alert("hola soy un mensaje")
